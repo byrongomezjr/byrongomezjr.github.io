2f9879824facd8cd50de3368742f9ad959a5c41e
Byron Gomez Jr <byrongomezjr@protonmail.com>
2022-10-03 22:18:31 -0400

    added tailwind css and motion framer, built header.

---
+++ components/Header.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <header className="sticky top-0 p-4 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center"> {/* mx-auto allows items to relocate to middle */}
+        <motion.div 
+        initial={{
+            x: -500,
+            opacity: 0,
+            scale: 0.5,
+        }}
+        animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+        }}
+        transition={{
+            duration: 1.5,
+        }}
+        className="flex flex-row items-center">
+            { /* Social Icons */ }
+        <SocialIcon url="https://github.com/byrongomezjr" fgColor="black" bgColor="transparent"/>
+        <SocialIcon url="https://instagram.com/byrongomezjr" fgColor="black" bgColor="transparent"/>
+        <SocialIcon url="https://linkedin.com/in/byrongomezjr" fgColor="black" bgColor="transparent"/>
+        </motion.div>
+
+        <motion.div
+        initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+        }}
+        animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+        }}
+        transition={{
+            duration: 1,
+        }}
+        className="flex flex-row items-center text-black cursor-pointer">
+            <SocialIcon 
+            className="cursor-pointer"
+            network="email"
+            fgColor="black"
+            bgColor="transparent"
+            />
+            <p className="uppercase hidden md:inline-flex text-sm text-black">Get In Touch</p>
+        </motion.div>
+    </header>
+  )
+}
