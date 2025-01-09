8b47203f8db148472dbad6bf1c3b94aa7319bbf7
Byron Gomez Jr <byrongomezjr@protonmail.com>
2025-01-09 13:36:39 -0500

    handle large files.

--- next.config.js
+++ next.config.js
@@ -5,14 +5,13 @@ const nextConfig = {
     unoptimized: true,
   },
   productionBrowserSourceMaps: false,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   swcMinify: true,
   compress: true,
   typescript: {
     ignoreBuildErrors: true,
   },
   webpack: (config, { isServer }) => {
+    // Minimize all JavaScript
     config.optimization.minimize = true;
 
     if (!isServer) {
@@ -20,7 +19,25 @@ const nextConfig = {
         chunks: 'all',
         maxInitialRequests: 25,
         minSize: 20000,
-        maxSize: 1000000,
+        maxSize: 500000, // Reduced from 1MB to 500KB
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          commons: {
+            name: 'commons',
+            chunks: 'all',
+            minChunks: 2,
+            reuseExistingChunk: true,
+          },
+          lib: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `npm.${packageName.replace('@', '')}`;
+            },
+            priority: 20,
+          },
+        },
       };
     }
 
